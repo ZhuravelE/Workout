8c0f67053d5e2d1225ae153d41fc832de6dbb837
ZhuravelE <zhuravel.evgenij@gmail.com>
2022-01-23 18:13:49 +0200

    Removed unnecessary array.

--- algorithms/src/main/java/com/zhuravel/SearchMinAndMaxInArray.java
+++ algorithms/src/main/java/com/zhuravel/SearchMinAndMaxInArray.java
@@ -3,7 +3,6 @@ package com.zhuravel;
 public class SearchMinAndMaxInArray {
 
     public static void main(String[] args) {
-        int[] arraySmall = {20, 91, 64, 75, 19, 26, 48, 55, 89};
         int[] arrayRandom = {70, 79, 98, 13, 75, 96, 24, 36, 88, 85, 47, 85, 56, 32, 5, 69, 86, 45, 82};
 
         printMaximumElement(arrayRandom);
