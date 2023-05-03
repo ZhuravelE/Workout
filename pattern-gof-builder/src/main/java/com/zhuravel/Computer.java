ff1d6e968518a7f9da7f85a026a5f0cb635413f8
ZhuravelE <zhuravel.evgenij@gmail.com>
2023-05-03 22:50:59 +0300

    Added Pattern-GOF-Builder module.

---
+++ pattern-gof-builder/src/main/java/com/zhuravel/Computer.java
@@ -0,0 +1,65 @@
+package com.zhuravel;
+
+/**
+ * @author Evgenii Zhuravel created on 03.05.2023
+ * @version 1.0
+ */
+public class Computer {
+
+    private final String HDD;
+    private final String RAM;
+
+    private final boolean isGraphicsCardEnabled;
+    private final boolean isBluetoothEnabled;
+
+    public String getHDD() {
+        return HDD;
+    }
+
+    public String getRAM() {
+        return RAM;
+    }
+
+    public boolean isGraphicsCardEnabled() {
+        return isGraphicsCardEnabled;
+    }
+
+    public boolean isBluetoothEnabled() {
+        return isBluetoothEnabled;
+    }
+
+    private Computer(ComputerBuilder builder) {
+        this.HDD = builder.HDD;
+        this.RAM = builder.RAM;
+        this.isGraphicsCardEnabled = builder.isGraphicsCardEnabled;
+        this.isBluetoothEnabled = builder.isBluetoothEnabled;
+    }
+
+    public static class ComputerBuilder {
+
+        private final String HDD;
+        private final String RAM;
+
+        private boolean isGraphicsCardEnabled;
+        private boolean isBluetoothEnabled;
+
+        public ComputerBuilder(String HDD, String RAM) {
+            this.HDD = HDD;
+            this.RAM = RAM;
+        }
+
+        public ComputerBuilder setGraphicsCardEnabled(boolean graphicsCardEnabled) {
+            isGraphicsCardEnabled = graphicsCardEnabled;
+            return this;
+        }
+
+        public ComputerBuilder setBluetoothEnabled(boolean bluetoothEnabled) {
+            isBluetoothEnabled = bluetoothEnabled;
+            return this;
+        }
+
+        public Computer build() {
+            return new Computer(this);
+        }
+    }
+}
